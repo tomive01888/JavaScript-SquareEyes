fb88baefeffaa96eba98fade3c99520144b2bfb4
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2024-04-23 08:22:54 +0200

    href fix

--- js/index-itemcard.js
+++ js/index-itemcard.js
@@ -1,6 +1,6 @@
 export function createHTML(movie) {  
 
-    let html = `<a class="movies-card" href="/product/index.html?movieid=${movie.id}">
+    let html = `<a class="movies-card" href="../product/index.html?movieid=${movie.id}">
                   <div class="flex-sale">
                     <p class="${movie.onSale ? "on-sale" : ""}">${movie.onSale ? movie.price : ""}</p>
                     <p class="current-price">$ ${movie.onSale ? movie.discountedPrice : movie.price} </p>
@@ -17,7 +17,7 @@ export function createHTML(movie) {
 
  export function createSmallCard(movie) {  
 
-    let html = `<a class="smallcard" href="/product/index.html?movieid=${movie.id}"> 
+    let html = `<a class="smallcard" href="../product/index.html?movieid=${movie.id}"> 
                   <img src="${movie.image.url}" alt="${movie.title}"/>
                 </a>`
  
